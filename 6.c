d8cc7fcaa958dbc259fa3e80398073ba8c35ede9
Alexandra <32397047+Littlesweetycupoftea@users.noreply.github.com>
2018-03-20 22:25:27 +0300

    Update 6.c

--- 6.c
+++ 6.c
@@ -5,6 +5,6 @@ int main(void)
 {
     setlocale(LC_ALL, "Rus");
     float x=2.55 ;
-    printf ("Результатом будет %f\n", 3*x*x*x - 5*x*x + 6);
+    printf ("Р РµР·СѓР»СЊС‚Р°С‚РѕРј Р±СѓРґРµС‚ %f\n", 3*x*x*x - 5*x*x + 6);
     return 0;
 }
