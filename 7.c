1e02372ac89b45649f0c4f6681617b5af2795468
Alexandra <32397047+Littlesweetycupoftea@users.noreply.github.com>
2018-03-20 22:26:13 +0300

    Update 7.c

--- 7.c
+++ 7.c
@@ -6,6 +6,6 @@ int main(void)
     setlocale(LC_ALL, "Rus");
     float x ;
     x = (3.31e-8*2.01e-7)/(7.16e-6 + 2.01e-8);
-    printf ("Результатом будет %e\n", x);
+    printf ("Р РµР·СѓР»СЊС‚Р°С‚РѕРј Р±СѓРґРµС‚ %e\n", x);
     return 0;
 }
